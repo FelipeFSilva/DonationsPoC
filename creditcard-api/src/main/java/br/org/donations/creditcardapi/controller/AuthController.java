bf2b67d9a32284a750f7b25cb6744bcaacc011f2
Felipe Silva <felipe_ffs6@hotmail.com>
2023-04-10 13:13:35 -0300

    Removendo código comentado/Passando o JWT para Authorization-api

--- creditcard-api/src/main/java/br/org/donations/creditcardapi/controller/AuthController.java
+++ creditcard-api/src/main/java/br/org/donations/creditcardapi/controller/AuthController.java
@@ -5,18 +5,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AuthController {
 
-//    private final JwtService jwtService;
-//    private final AuthenticationManager authenticationManager;
-//
-//    public AuthController(JwtService jwtService, AuthenticationManager authenticationManager) {
-//        this.jwtService = jwtService;
-//        this.authenticationManager = authenticationManager;
-//    }
-//
-//    @PostMapping("/token")
-//    public String getToken(@RequestBody LoginRequest userLogin) throws AuthenticationException {
-//
-//        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(userLogin.getUsername(), userLogin.getPassword()));
-//        return "";
-//    }
+
 }
