43a6075c8e757a16370a8a4b939245785cf76419
Felipe Silva <felipe_ffs6@hotmail.com>
2023-04-19 22:30:11 -0300

    alterando pra pegar o email do config

--- billet-api/src/main/java/br/org/donations/billetapi/service/EmailSenderService.java
+++ billet-api/src/main/java/br/org/donations/billetapi/service/EmailSenderService.java
@@ -1,28 +1,31 @@
 package br.org.donations.billetapi.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
-
+@Slf4j(topic = "EmailSenderService")
 @Service
 public class EmailSenderService {
 
-//    @Value("${spring.mail.username}")
-//    private String from;
+    @Value("${spring.mail.username}")
+    private String from;
 
     @Autowired
     private JavaMailSender emailSender;
 
     @Async
     public void sendEmailToDonor(String emailDonor) {
+        log.info("Iniciando o envio de email");
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("ffernandes286@gmail.com");
+        message.setFrom(from);
         message.setTo(emailDonor);
-        message.setSubject("Teste");
-        message.setText("Teste");
+        message.setSubject("Obrigado pela sua Doação");
+        message.setText("Obrigado pela sua doação");
         emailSender.send(message);
+        log.info("Finalizando o envio de email");
     }
 }
